fb02f982769e262cb14f08e66e1374331ddd3691
jeffersontads <jeffersontads@hotmail.com>
2021-10-04 15:30:36 -0300

    generating a static qr code

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 
     //vamos fazer o aruqivo conehcer o classe isso é a dependencia da classe payload
     use \App\Pix\Payload;
+    use \Mpdf\QrCode\Output;
+    use Mpdf\QrCode\QrCode;
 
     //chamada do metodo que define a chave pix da transacao
     $obPayload = (new Payload) 
@@ -10,14 +12,26 @@
     -> setDescription('Pagamento pedido SOTTO')
     -> setMerchantname('Jefferson Sotto')
     ->setMerchantCity('Foz do Iguaçu')
-    ->setAmount('900.00')
+    ->setAmount('1.00')
     ->setTxId('jeff123')
     ;
 
     // recebe o valor de get ele retorna a string do qrcode
+    //codigo de pagamento PIX
     $payLoadQrCode = $obPayload->getPayload();
+    $obQrCode = new QrCode($payLoadQrCode);
 
-    echo '<pre>';
-    print_r($payLoadQrCode);
-    echo '</pre>'; exit();
+//chamando uma das classes out para criar o qrcode usamos output image que vem junto com o mpdf qrcode e passamos o tamanho desta imagem
+$image = (new Output\Png)->output($obQrCode,400);
+
+
+//podemos imprimir a imagem do qrcode e tambem o codigo
 ?>
+
+<h1>QRCODE PIX</h1>
+<br>
+<img src="data:image/png;base64, <?php echo base64_encode($image); ?>">
+<br><br>
+
+Codigo PIX:<br>
+<strong><?php echo $payLoadQrCode; ?></strong>
